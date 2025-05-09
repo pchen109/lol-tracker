60614b8eeaeb375039819457cdefad76a768e07a
Snorlax <pchen109@my.bcit.ca>
2025-05-09 08:47:40 -0700

    modify receiver and storage

---
+++ lab6/receiver/app.py
@@ -0,0 +1,58 @@
+import connexion
+import threading
+import httpx
+import uuid
+import yaml
+import logging.config
+from pykafka import KafkaClient
+from datetime import datetime
+import json
+
+with open ("conf_app.yml", "r") as f:
+    variables = yaml.safe_load(f.read())
+url_activity = variables["urls"]["activity"]
+url_match = variables["urls"]["match"]
+hostname = variables["kafka"]["hostname"]
+port = variables["kafka"]["port"]
+topic_name = variables["kafka"]["topic"]
+
+with open("conf_log.yml", "r") as f:
+    log_setting = yaml.safe_load(f.read())
+    logging.config.dictConfig(log_setting)
+logger = logging.getLogger("basicLogger")
+
+def add_activity(body):
+    return add_event(body, "add_activity")
+
+def add_match(body):
+    return add_event(body, "add_match")
+
+def add_event(payload, event):
+    trace_id = str(uuid.uuid4())
+    payload["trace_id"] = trace_id
+    logger.info("RECEIVED - %s (trace id: %s)", event, trace_id)
+
+    client = KafkaClient(hosts=f"{hostname}:{port}")
+    topic = client.topics[str.encode(topic_name)]
+    producer = topic.get_sync_producer()
+
+    msg = {
+        "type": event,
+        "datetime": datetime.now().strftime("%Y-%m-%dT%H:%M:%S%z"),
+        "payload": payload
+    }
+
+    msg_str = json.dumps(msg)
+
+    producer.produce(msg_str.encode("utf-8"))
+    logger.info("RESPONSE - %s (trace id: %s) has status %s", event, trace_id, 201)
+    return msg, 201
+    
+
+app = connexion.FlaskApp(__name__, specification_dir='')
+app.add_api("openapi.yml",
+            strict_validation=True,
+            validate_responses=True)
+
+if __name__ == "__main__":
+    app.run(port=8080)
---
+++ lab6/storage/app.py
@@ -0,0 +1,135 @@
+import connexion
+from connexion import NoContent
+from datetime import datetime as dt
+from sqlalchemy import select
+import threading
+from db_init import make_session
+from db_model import UserActivity, UserMatch
+from db_mgmt import create_tables, drop_tables
+import yaml
+import logging.config
+from pykafka import KafkaClient
+from pykafka.common import OffsetType
+import json
+from threading import Thread
+
+with open ("conf_app.yml", "r") as f:
+    variables = yaml.safe_load(f.read())
+hostname = variables["kafka"]["hostname"]
+port = variables["kafka"]["port"]
+topic_name = variables["kafka"]["topic"]
+
+with open("conf_log.yml", "r") as f:
+    log_setting = yaml.safe_load(f.read())
+    logging.config.dictConfig(log_setting)
+logger = logging.getLogger("basicLogger")
+
+db_lock = threading.Lock()
+
+def add_activity(body):
+    with db_lock:
+        store_events(body)
+    trace_id = body["trace_id"]
+    logger.debug(f"SUCCESS DATABASE - add_activity (trace id: {trace_id})")
+    return NoContent, 201
+
+def add_match(body):
+    with db_lock:
+        store_events(body)
+    trace_id = body["trace_id"]
+    logger.debug(f"SUCCESS DATABASE - add_match (trace id: {trace_id})")
+    return NoContent, 201
+
+def store_events(payload):
+    session = make_session()
+    if "region" in payload:
+        event = UserActivity(
+            user_id = payload["user_id"],
+            region = payload["region"],
+            login_counts = payload["login_counts"],
+            timestamp = dt.strptime(payload["timestamp"], "%Y-%m-%dT%I:%M:%S.%f%z"),
+            trace_id = payload["trace_id"],
+        )
+    if "match_id" in payload:
+        event = UserMatch(
+            match_id = payload["match_id"],
+            user_id = payload["user_id"],
+            kill = payload["kill"],
+            death = payload["death"],
+            assist = payload["assist"],
+            timestamp = dt.strptime(payload["timestamp"], "%Y-%m-%dT%I:%M:%S.%f%z"),
+            trace_id = payload["trace_id"],
+        )
+    session.add(event)
+    session.commit()
+    session.close()
+
+### KAFKA Consume
+def process_messages():
+    client = KafkaClient(hosts=f"{hostname}:{port}")
+    topic = client.topics[str.encode(topic_name)]
+
+    consumer = topic.get_simple_consumer(
+        consumer_group= b'event_group',
+        rest_offset_on_start = False,
+        auto_offset_rest = OffsetType.LATEST
+    )
+
+    for msg in consumer:
+        msg_str = msg.value.decode("utf-8")
+        msg = json.load(msg_str)
+
+        payload = msg["payload"]
+
+        if msg["type"] == "add_activity":
+            add_activity(payload)
+        elif msg["type"] == "add_match":
+            add_match(payload)
+        consumer.commit_offsets
+### End of KAFKA Consume
+
+### THREAD
+def setup_kafka_thread():
+    t1 = Thread(target=process_messages)
+    t1.setDaemon(True)
+    t1.start()
+### End of THREAD 
+
+### GET Method
+def get_activity(start_timestamp, end_timestamp):
+    return get_events(start_timestamp, end_timestamp, UserActivity)
+
+def get_match(start_timestamp, end_timestamp):
+    return get_events(start_timestamp, end_timestamp, UserMatch)
+
+def get_events(start_timestamp, end_timestamp, class_name): 
+    session = make_session()
+    start = dt.fromisoformat(start_timestamp)
+    end = dt.fromisoformat(end_timestamp)
+
+    statement = select(class_name)\
+        .where(class_name.date_created >= start)\
+        .where(class_name.date_created < end)
+    
+    results = [
+        result.to_dict() for result in session.execute(statement).scalars().all()
+    ]
+    
+    session.close()
+    logger.info("Found %d %s event (start: %s, end: %s)", len(results), class_name.__table__.name , start, end)
+    logger.debug(f"{type(results)}")
+    return results, 200
+### End of GET Method
+
+
+
+app = connexion.FlaskApp(__name__, specification_dir='')
+app.add_api("openapi.yml",
+            strict_validation=True,
+            validate_responses=True)
+
+if __name__ == "__main__":
+    drop_tables()     ### used for debug only
+    create_tables()
+    setup_kafka_thread()
+    app.run(port=8090)
